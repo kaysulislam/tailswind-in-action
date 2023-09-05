c0a4032919c9ebcf98e85027ed8d65a2c0a1ac58
kaysul <kaysul.islam@distence.fi>
2023-09-05 18:42:30 +0300

    Added homepage

---
+++ src/sections/CurstomerReviews.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CurstomerReviews = () => {
+ return <div>CurstomerReviews</div>
+}
+
+export default CurstomerReviews
---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SuperQuality from './SuperQuality'
+import SpecialOffers from './SpecialOffers'
+import CurstomerReviews from './CurstomerReviews'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+
+export { Hero, PopularProducts, Services, SuperQuality, SpecialOffers, CurstomerReviews, Subscribe, Footer }
